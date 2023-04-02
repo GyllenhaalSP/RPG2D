4908d8d1f5592f334816afb536b9cbb02fb07788
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-04-02 12:25:14 +0200

    Initial commit.

---
+++ src/KeyHandler.java
@@ -0,0 +1,59 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    public boolean upPressed;
+    public boolean downPressed;
+    public boolean leftPressed;
+    public boolean rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_UP || code == KeyEvent.VK_W) {
+            upPressed = true;
+            System.out.println("UP");
+        }
+
+        if (code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S) {
+            downPressed = true;
+            System.out.println("DOWN");
+        }
+
+        if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A) {
+            leftPressed = true;
+            System.out.println("LEFT");
+        }
+
+        if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
+            rightPressed = true;
+            System.out.println("RIGHT");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_UP || code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+
+        if (code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+
+        if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+
+        if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+
+    }
+}
