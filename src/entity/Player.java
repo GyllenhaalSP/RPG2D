df1a315169bc0940eca900ea849d2b4dea5f220f
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-04-03 05:34:03 +0200

    Declared two new fields screenX and screenY and initialized them. Renamed x and y to worldX and worldY. Wrapped getResourceAsStream() with requireNonNull() from Objects. Added condition in update() to move while counting sprites in order to alternate between walking sprites. Added logic for walking sprites in draw().

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -7,69 +7,99 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity{
 
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+        screenX = gp.SCREEN_WIDTH / 2 - (gp.TILE_SIZE / 2);
+        screenY = gp.SCREEN_HEIGHT / 2 - (gp.TILE_SIZE / 2);
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues(){
-        x = 100;
-        y = 100;
+        worldX = gp.TILE_SIZE * 23;
+        worldY = gp.TILE_SIZE * 21;
         speed = 5;
         direction = "down";
     }
 
     public void getPlayerImage(){
         try {
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void update() {
+        if(keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed){
+            move();
+        }
+    }
+
+    public void move(){
         if (keyH.upPressed) {
             direction = "up";
-            y -= speed;
+            worldY -= speed;
         } else if (keyH.downPressed) {
             direction = "down";
-            y += speed;
+            worldY += speed;
         } else if (keyH.leftPressed) {
             direction = "left";
-            x -= speed;
+            worldX -= speed;
         } else if (keyH.rightPressed) {
             direction = "right";
-            x += speed;
+            worldX += speed;
+        }
+
+        spriteCounter++;
+        if(spriteCounter > 15){
+            if(spriteNum == 1) spriteNum = 2;
+            else if(spriteNum == 2) spriteNum = 1;
+            spriteCounter = 0;
         }
     }
 
     public void draw(Graphics2D g2) {
-//        g2.setColor(Color.WHITE);
-//        g2.fillRect(x, y, gp.TILE_SIZE, gp.TILE_SIZE);
-        BufferedImage image = switch (direction) {
-            case "up" -> up1;
-            case "down" -> down1;
-            case "left" -> left1;
-            case "right" -> right1;
-            default -> null;
-        };
+        BufferedImage image = null;
 
-        g2.drawImage(image, x, y, gp.TILE_SIZE, gp.TILE_SIZE, null);
+        switch (direction) {
+            case "up" -> {
+                if (spriteNum == 1) image = up1;
+                if (spriteNum == 2) image = up2;
+            }
+            case "down" -> {
+                if (spriteNum == 1) image = down1;
+                if (spriteNum == 2) image = down2;
+            }
+            case "left" -> {
+                if (spriteNum == 1) image = left1;
+                if (spriteNum == 2) image = left2;
+            }
+            case "right" -> {
+                if (spriteNum == 1) image = right1;
+                if (spriteNum == 2) image = right2;
+            }
+        }
+
+        g2.drawImage(image, screenX, screenY, gp.TILE_SIZE, gp.TILE_SIZE, null);
 
 
 
