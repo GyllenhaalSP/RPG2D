31e6c864184226616bef3c0ab09d539973fd2b96
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-04-09 17:17:27 +0200

    Added GamePanel to fields. Added constructor. Added key event for pause.

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -5,6 +5,7 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
 
+    GamePanel gp;
     public boolean upPressed;
     public boolean downPressed;
     public boolean leftPressed;
@@ -12,6 +13,10 @@ public class KeyHandler implements KeyListener {
 
     // DEBUG
     boolean checkDrawTime = false;
+
+    public KeyHandler(GamePanel gp) {
+        this.gp = gp;
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -38,6 +43,15 @@ public class KeyHandler implements KeyListener {
         if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
             rightPressed = true;
             //System.out.println("RIGHT");
+        }
+
+        // PAUSE
+        if (code == KeyEvent.VK_P) {
+            if(gp.gameState == gp.playState){
+                gp.gameState = gp.pauseState;
+            } else if (gp.gameState == gp.pauseState){
+                gp.gameState = gp.playState;
+            }
         }
 
         // DEBUG
