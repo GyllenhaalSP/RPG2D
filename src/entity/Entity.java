e5bcc9a93960b87ad9f61e293d9997ce51ec96c1
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-04-12 04:09:42 +0200

    Added imports. Added GamePanel field. Initialized solidArea to collision rectangle the same size as Player. Added logic to manage entity movement, collision, drawing and setup.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,13 +1,18 @@
 package entity;
 
+import main.GamePanel;
+import main.UtilityTools;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Entity {
     public int worldX;
     public int worldY;
     public int speed;
-
     public BufferedImage up1;
     public BufferedImage up2;
     public BufferedImage down1;
@@ -16,15 +21,90 @@ public class Entity {
     public BufferedImage left2;
     public BufferedImage right1;
     public BufferedImage right2;
-
+    public String direction;
     public int spriteCounter = 0;
     public int spriteNum = 1;
-
-    public Rectangle solidArea;
+    public Rectangle solidArea = new Rectangle(8, 15, 32, 32);
     public int solidAreaDefaultX;
     public int solidAreaDefaultY;
     public boolean collisionON = false;
+    public int actionLockCounter = 0;
+    GamePanel gp;
 
-    public String direction;
+    public Entity(GamePanel gp) {
+        this.gp = gp;
+    }
 
+    public void setAction() {
+
+    }
+
+    public void update() {
+
+        setAction();
+
+        collisionON = false;
+        gp.collisionChecker.checkTile(this);
+        gp.collisionChecker.checkObject(this, false);
+        gp.collisionChecker.checkPlayer(this);
+
+        if (!collisionON) {
+            switch (direction) {
+                case "up" -> worldY -= speed;
+                case "down" -> worldY += speed;
+                case "left" -> worldX -= speed;
+                case "right" -> worldX += speed;
+            }
+        }
+
+        spriteCounter++;
+        if (spriteCounter > 15) {
+            if (spriteNum == 1) spriteNum = 2;
+            else if (spriteNum == 2) spriteNum = 1;
+            spriteCounter = 0;
+        }
+    }
+
+    public BufferedImage setup(String imagePath) {
+        UtilityTools utilityTools = new UtilityTools();
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(imagePath + ".png")));
+            image = utilityTools.scaleImage(image, gp.TILE_SIZE, gp.TILE_SIZE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    public void draw(Graphics2D g2) {
+        BufferedImage image = null;
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        if (worldX + gp.TILE_SIZE > gp.player.worldX - gp.player.screenX && worldX - gp.TILE_SIZE < gp.player.worldX + gp.player.screenX && worldY + gp.TILE_SIZE > gp.player.worldY - gp.player.screenY && worldY - gp.TILE_SIZE < gp.player.worldY + gp.player.screenY) {
+
+            switch (direction) {
+                case "up" -> {
+                    if (spriteNum == 1) image = up1;
+                    if (spriteNum == 2) image = up2;
+                }
+                case "down" -> {
+                    if (spriteNum == 1) image = down1;
+                    if (spriteNum == 2) image = down2;
+                }
+                case "left" -> {
+                    if (spriteNum == 1) image = left1;
+                    if (spriteNum == 2) image = left2;
+                }
+                case "right" -> {
+                    if (spriteNum == 1) image = right1;
+                    if (spriteNum == 2) image = right2;
+                }
+            }
+
+            g2.drawImage(image, screenX, screenY, null);
+        }
+    }
 }
