a22bfe837bd08a8ae073b114dfab9b561d82cd46
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-04-12 04:10:45 +0200

    Added setNPC().

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.NPC_OldMan;
+
 public class AssetSetter {
     GamePanel gp;
 
@@ -8,5 +10,11 @@ public class AssetSetter {
     }
 
     public void setObject() {
+    }
+
+    public void setNPC() {
+        gp.npcs[0] = new NPC_OldMan(gp);
+        gp.npcs[0].worldX = gp.TILE_SIZE*21;
+        gp.npcs[0].worldY = gp.TILE_SIZE*21;
     }
 }
